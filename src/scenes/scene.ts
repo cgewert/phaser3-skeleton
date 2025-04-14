10a1aa304cc045fa4e9d0e5197908027cccb1d36
cgewert <cgewert@gmail.com>
2025-04-14 21:35:46 +0200

    Optimize tsconfig file

--- src/scenes/scene.ts
+++ src/scenes/scene.ts
@@ -1,39 +1,37 @@
-import * as PHASER from 'phaser';
+import * as PHASER from "phaser";
 
 export class Scene extends PHASER.Scene {
-    private static CONFIG: Phaser.Types.Scenes.SettingsConfig = {
-        key: 'scene'
-    }
+  private static CONFIG: Phaser.Types.Scenes.SettingsConfig = {
+    key: "scene",
+  };
 
-    private gameOver: boolean;
+  private gameOver: boolean = false;
 
-    constructor() {
-        super(Scene.CONFIG);
-    }
+  constructor() {
+    super(Scene.CONFIG);
+  }
 
-    /* Scene initialization logic. */
-    public init(data: any) {
+  /* Scene initialization logic. */
+  public init(data: any) {}
 
-    }
+  public preload() {
+    // Load scene assets here.
+  }
 
-    public preload() {
-        // Load scene assets here.
-    }
+  /*
+   * Create your game objects here.
+   * @param data - Custom data provided to the scene.
+   */
+  public create(data: any) {
+    // Create game objects here.
+  }
 
-    /* 
-     * Create your game objects here.
-     * @param data - Custom data provided to the scene.
-    */
-    public create(data: any) {
-        // Create game objects here.
-    }
- 
-    /**
-     * Updates the scene logic.
-     * @param time - Overall time in ms since game started.
-     * @param delta - Time in ms since last update call.
-     */
-    public update(time: number, delta: number) {
-        // Update the game logic here.
-    }
+  /**
+   * Updates the scene logic.
+   * @param time - Overall time in ms since game started.
+   * @param delta - Time in ms since last update call.
+   */
+  public update(time: number, delta: number) {
+    // Update the game logic here.
+  }
 }
